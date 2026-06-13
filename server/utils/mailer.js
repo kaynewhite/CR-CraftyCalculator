b02abea061cb8bc9e7dc45b5e3446813ea819b60
Ron Religioso <111487700+kaynewhite@users.noreply.github.com>
2026-06-13 18:34:02 +0000

    port fix HAHAHAHAH lala

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -2,8 +2,8 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || 'smtp-relay.brevo.com',
-  port: parseInt(process.env.SMTP_PORT || '587'),
-  secure: false,
+  port: parseInt(process.env.SMTP_PORT || '587' || '465'),
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
